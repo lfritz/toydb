16ccb1fb6e4419712bffb4a99d9cf9f299266101
Levin Fritz <levin.fritz@gmail.com>
2022-10-02 12:16:15 +0200

    Merge error.go and errors.go

--- sql/errors.go
+++ sql/errors.go
@@ -3,3 +3,12 @@ package sql
 import "errors"
 
 var NotImplemented = errors.New("not yet implemented")
+
+type SyntaxError struct {
+	Position int
+	Msg      string
+}
+
+func (e SyntaxError) Error() string {
+	return e.Msg
+}
