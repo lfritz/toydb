002d6dfdc77a68c6aab28411ebfaf56ab2c371be
Levin Fritz <levin.fritz@gmail.com>
2022-10-09 15:29:53 +0200

    Add types for relations and schemas

--- types/relation.go
+++ types/relation.go
@@ -1,3 +1,19 @@
 package types
 
-// TODO add a type Relation
+// A Relation represents a table or the result of a query.
+type Relation struct {
+	Schema TableSchema
+	Rows   [][]Value
+}
+
+func (r *Relation) Row(i int) *Row {
+	return &Row{
+		Schema: r.Schema,
+		Values: r.Rows[i],
+	}
+}
+
+type Row struct {
+	Schema TableSchema
+	Values []Value
+}
--- types/schema.go
+++ types/schema.go
@@ -1,3 +1,21 @@
 package types
 
-// TODO add types TableSchema and ColumnSchema
+import "fmt"
+
+type TableSchema struct {
+	Columns []ColumnSchema
+}
+
+func (s TableSchema) Column(name string) (i int, t Type) {
+	for i, c := range s.Columns {
+		if c.Name == name {
+			return i, c.Type
+		}
+	}
+	panic(fmt.Sprintf("column not found: %s", name))
+}
+
+type ColumnSchema struct {
+	Name string
+	Type Type
+}
