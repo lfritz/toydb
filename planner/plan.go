746b7465572213d142b8931d87bd32d77d181638
Levin Fritz <levin.fritz@gmail.com>
2022-10-14 07:35:24 +0200

    Query planner: implement simplest case

--- planner/plan.go
+++ planner/plan.go
@@ -1,13 +1,40 @@
 package planner
 
 import (
+	"errors"
+
 	"github.com/lfritz/toydb/query"
 	"github.com/lfritz/toydb/sql"
 	"github.com/lfritz/toydb/storage"
 )
 
+var NotImplemented = errors.New("not implemented")
+
 // Plan creates a query plan for the query.
-func Plan(query *sql.SelectStatement, db *storage.Database) (*query.Plan, error) {
-	// TODO
-	return nil, nil
+func Plan(stmt *sql.SelectStatement, db *storage.Database) (query.Plan, error) {
+	var plan query.Plan
+
+	switch f := stmt.From.(type) {
+	case sql.TableName:
+		table, err := db.Table(f.Name)
+		if err != nil {
+			return nil, err
+		}
+		plan = query.NewLoad(f.Name, table.Schema)
+	default:
+		return nil, NotImplemented
+	}
+
+	switch stmt.What.(type) {
+	case sql.Star:
+		// ok
+	default:
+		return nil, NotImplemented
+	}
+
+	if stmt.Where != nil {
+		return nil, NotImplemented
+	}
+
+	return plan, nil
 }
--- planner/plan_test.go
+++ planner/plan_test.go
@@ -1,9 +1,47 @@
 package planner
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/lfritz/toydb/query"
+	"github.com/lfritz/toydb/sql"
+	"github.com/lfritz/toydb/storage"
 )
 
+func parse(t *testing.T, input string) *sql.SelectStatement {
+	statement, err := sql.Parse(input)
+	if err != nil {
+		t.Fatalf("sql.Parse returned error for %q: %v", input, err)
+	}
+	selectStatement, ok := statement.(*sql.SelectStatement)
+	if !ok {
+		t.Fatalf("Not a select statement: %q", input)
+	}
+	return selectStatement
+}
+
 func TestPlan(t *testing.T) {
-	// TODO
+	sampleData := storage.GetSampleData()
+
+	cases := []struct {
+		stmt string
+		want query.Plan
+	}{
+		{
+			"select * from films",
+			query.NewLoad("films", sampleData.Films.Schema),
+		},
+	}
+
+	for _, c := range cases {
+		stmt := parse(t, c.stmt)
+		got, err := Plan(stmt, sampleData.Database)
+		if err != nil {
+			t.Fatalf("Plan returned error: %v", err)
+		}
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Query plan for\n%s\nis:\n%q\nwant:\n%v\n", c.stmt, got, c.want)
+		}
+	}
 }
